13e18b1e33eae2a99e5de7db45f02e4e83a890c1
Wil-dev <mehdididou7@icloud.com>
2025-01-29 18:36:43 +0100

    added signin in front side

--- src/app/(auth)/signin/page.jsx
+++ src/app/(auth)/signin/page.jsx
@@ -1,5 +1,44 @@
+"use client";
+import { useRef } from "react";
+
 export default function page() {
-  return (
-    <div><h1 className="mt-20">Sign In</h1></div>
-  )
+	const serverInfoRef = useRef();
+	const submitButtonRef = useRef();
+
+	async function handleSubmit(e) {
+		e.preventDefault();
+	}
+	return (
+		<form className="max-w-md mx-auto mt-36" onSubmit={handleSubmit}>
+			<label htmlFor="userName" className="f-label">
+				Your pseudo
+			</label>
+			<input
+				type="text"
+				className="f-auth-input"
+				id="userName"
+				name="userName"
+				required
+				placeholder="Your pseudo"
+			/>
+			<label htmlFor="password" className="f-label">
+				Your password
+			</label>
+			<input
+				type="password"
+				className="f-auth-input"
+				id="password"
+				name="password"
+				required
+				placeholder="Your password"
+			/>
+			<button
+				ref={submitButtonRef}
+				className="w-full bg-indigo-500 hover:bg-indigo-700 text-white font-bold py-3 px-4 mt-6 mb-10 rounded border-none"
+			>
+				Submit
+			</button>
+			<p ref={serverInfoRef} className="text-center mb-10"></p>
+		</form>
+	);
 }
