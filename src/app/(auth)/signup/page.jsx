686ddf530f821900881722febd5383a0894aeb17
Wil-dev <mehdididou7@icloud.com>
2025-01-29 18:23:23 +0100

    sending the login form to the database and redirecting to sign up

--- src/app/(auth)/signup/page.jsx
+++ src/app/(auth)/signup/page.jsx
@@ -1,6 +1,52 @@
+"use client";
+import { register } from "@/lib/serverActions/session/sessionServerActions";
+import { useRef } from "react";
+import { useRouter } from "next/navigation";
+
 export default function page() {
+	const serverInfoRef = useRef();
+	const submitButtonRef = useRef();
+  const router = useRouter();
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+
+    serverInfoRef.current.classList.add("hidden");
+    serverInfoRef.current.textContent = ""
+    submitButtonRef.current.textContent = "Saving User"
+    submitButtonRef.current.disabled = true;
+
+   try {
+        const result = await register(new FormData(e.target));
+   
+        if (result.success) {
+          submitButtonRef.current.textContent = "User created ✅";
+   
+          let countdown = 3;
+
+          serverInfoRef.current.classList.remove("hidden");
+
+          serverInfoRef.current.textContent = `Redirecting in ${countdown} seconds...`;
+          const interval = setInterval(() => {
+            countdown -= 1;
+            serverInfoRef.current.textContent = `Redirecting in ${countdown} seconds...`;
+            if (countdown === 0) {
+              clearInterval(interval);
+              router.push(`/signin`);
+            }
+          }, 1000);
+        }
+      } catch (error) {
+        serverInfoRef.current.textContent = "Submit";
+        submitButtonRef.current.textContent = `${error.message}`
+        submitButtonRef.current.disabled = false;
+      }
+    }
+
 	return (
-		<form className="max-w-md mx-auto mt-36">
+		<form 
+    onSubmit={handleSubmit}
+    className="max-w-md mx-auto mt-36">
 			<label htmlFor="userName" className="f-label">
 				Name or Pseudo
 			</label>
@@ -46,13 +92,22 @@ export default function page() {
 				required
 			/>
 
-			<button className="w-full bg-indigo-500 hover:bg-indigo-700 text-white font-bold py-3 px-4 my-10 rounded border-none">
+			<button
+				ref={submitButtonRef}
+				className="w-full bg-indigo-500 hover:bg-indigo-700 text-white font-bold py-3 px-4 my-10 rounded border-none"
+			>
 				Submit
 			</button>
+      <p ref={serverInfoRef}
+          className="hidden text-center mb-10"
+      ></p>
 
-      <a href="/signin"
-        className="mb-5 underline text-blue-600 block text-center"
-      >Already have an account ? Log in</a>
+			<a
+				href="/signin"
+				className="mb-5 underline text-blue-600 block text-center"
+			>
+				Already have an account ? Log in
+			</a>
 		</form>
 	);
 }
---
+++ src/lib/serverActions/session/sessionServerActions.js
@@ -0,0 +1,56 @@
+"use server";
+import { connectToDB } from "@/lib/utils/db/connectToDB";
+import { connect } from "mongoose";
+import slugify from "slugify";
+import { User } from "@/lib/models/user";
+import bcrypt from "bcryptjs";
+
+export async function register(formData) {
+	const { userName, email, password, passwordRepeat } =
+		Object.fromEntries(formData); // On ne peut pas faire du destrtucuting de formData ce n est pas un obj iterable
+
+	if (userName.lenght < 3) {
+		throw new Error("Username is too short");
+	}
+
+	if (password.length < 6) {
+		throw new Error("Password is too short");
+	}
+
+	if (password !== passwordRepeat) {
+		throw new Error("Passwords do not match");
+	}
+
+	try {
+		connectToDB();
+		const user = await User.findOne({ userName });
+
+		if (user) {
+			throw new Error("Username already exists");
+		}
+    const normalizedUserName = slugify(userName, { lower: true, strict: true });
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const newUser = new User({
+      userName,
+      normalizedUserName,
+      email,
+      password: hashedPassword,
+    });
+
+    await newUser.save();
+
+    console.log("User saved to db");
+    
+
+
+    return {success: true}
+
+	} catch (error) {
+    console.log("Error while creating the user:", error);
+    throw new Error(error.message || "An error occured while creating the user");
+    
+  }
+}
