a3da74c62b694c5fc3a09fd08b7fb536844d9ff9
Wil-dev <mehdididou7@icloud.com>
2025-01-29 19:40:31 +0100

    creating the server action login

--- src/lib/serverActions/session/sessionServerActions.js
+++ src/lib/serverActions/session/sessionServerActions.js
@@ -1,9 +1,10 @@
 "use server";
 import { connectToDB } from "@/lib/utils/db/connectToDB";
-import { connect } from "mongoose";
 import slugify from "slugify";
 import { User } from "@/lib/models/user";
 import bcrypt from "bcryptjs";
+import { Session } from "@/lib/models/session";
+import { cookies } from "next/headers";
 
 export async function register(formData) {
 	const { userName, email, password, passwordRepeat } =
@@ -28,29 +29,78 @@ export async function register(formData) {
 		if (user) {
 			throw new Error("Username already exists");
 		}
-    const normalizedUserName = slugify(userName, { lower: true, strict: true });
+		const normalizedUserName = slugify(userName, { lower: true, strict: true });
 
-    const salt = await bcrypt.genSalt(10);
-    const hashedPassword = await bcrypt.hash(password, salt);
+		const salt = await bcrypt.genSalt(10);
+		const hashedPassword = await bcrypt.hash(password, salt);
 
-    const newUser = new User({
-      userName,
-      normalizedUserName,
-      email,
-      password: hashedPassword,
-    });
+		const newUser = new User({
+			userName,
+			normalizedUserName,
+			email,
+			password: hashedPassword,
+		});
 
-    await newUser.save();
+		await newUser.save();
 
-    console.log("User saved to db");
-    
+		console.log("User saved to db");
 
+		return { success: true };
+	} catch (error) {
+		console.log("Error while creating the user:", error);
+		throw new Error(
+			error.message || "An error occured while creating the user"
+		);
+	}
+}
 
-    return {success: true}
+export async function login(formData) {
+	// const { userName, password } = Object.fromEntries(formData);  ou bien
+	const userName = formData.get("userName");
+	const password = formData.get("password");
+
+	try {
+		await connectToDB();
+
+		const user = await User.findOne({ userName });
+		if (!user) {
+			throw new Error("Invalid credentials"); // on dit cela parce que l on ne sait pas si c est le userName ou le mdp qui est faux
+		}
+		const isPasswordValid = await bcrypt.compare(password, user.password);
+		if (!isPasswordValid) {
+			throw new Error("Invalid credentials");
+		}
+
+		const existingSession = await Session.findOne({
+			userId: user._id,
+			expiresAt: { $gt: Date.now() },
+		});
+		if (existingSession) return { success: true };
+
+		const session = new Session({
+			userId: user._id,
+			expiresAt: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000), // 7 days
+		});
+
+		await session.save();
+
+		const cookieStore = cookies()
+		cookieStore.set("sessionId", session._id.toString(), {
+			httpOnly: true,
+			secure: process.env.NODE_ENV === "production",
+			path: "/",
+			maxAge: 7 * 24 *60 * 60,
+			sameSite: "Lax" // Attaques CSRF
+		})
+
+		return {success : true}
 
 	} catch (error) {
-    console.log("Error while creating the user:", error);
-    throw new Error(error.message || "An error occured while creating the user");
-    
-  }
+		console.log("Error while signing in the user :",error);
+
+		throw new Error(
+			error.message || "An error occured while signing in the user"
+		);
+		
+	}
 }
