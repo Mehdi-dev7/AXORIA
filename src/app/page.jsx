76d220595175abe07d5faf722a0f2a2649df9c04
Wil-dev <mehdididou7@icloud.com>
2025-04-23 20:09:02 +0200

    Intégration du composant BlogCard pour améliorer l'affichage des articles sur la page d'accueil, simplifiant ainsi le code et la présentation des posts.

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,7 +1,7 @@
 import Link from "next/link";
 import { connectToDB } from "@/lib/utils/db/connectToDB";
 import { getPosts } from "@/lib/serverMethods/blog/postMethods";
-
+import BlogCard from "@/components/BlogCard";
 export default async function Home() {
 
 	const posts = await getPosts();
@@ -15,21 +15,7 @@ export default async function Home() {
 			<p className="text-md text-zinc-900">Latest articles</p>
 			<ul className="u-article-grid">
         {posts.map((post, id) => (
-          <li key={id} className="rounded-sm shadow-md hover:shadow-xl border hover:border-zinc-300">
-            <div className="pt-5 px-5 pb-7">
-              <div className="flex items-baseline gap-x-4 text-xs">
-                <time dateTime={new Date().toISOString()} className="text-gray-500 text-sm">
-                  {new Date().toLocaleDateString("en-EN", {year: "numeric", month: "long", day: "numeric"})}
-                </time>
-                <Link href={`/categories/author/johndoe`} className="ml-auto text-base text-gray-700 hover:text-gray-600 whitespace-nowrap truncate">
-                  Jonh Doe
-                </Link>
-              </div>
-              <Link href={`/article/${post.slug}`} className="inline-block mt-6 text-xl font-semibold text-zinc-800 hover:text-zinc-600">
-                {post.title}
-              </Link>
-            </div>
-          </li>
+          <BlogCard key={id} post={post} />
         ))}
       </ul>
 		</div>
