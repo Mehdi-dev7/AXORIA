07253d6c63f1f48eb23f73d1e8451215ed51eb9f
Wil-dev <mehdididou7@icloud.com>
2025-04-28 18:37:36 +0200

    Amélioration de la page d'édition d'article avec l'ajout de la gestion des erreurs lors de la soumission du formulaire, mise à jour des messages d'erreur pour une meilleure clarté, et optimisation de la validation des champs requis.

---
+++ src/app/api/auth/validateSession/route.js
@@ -0,0 +1,37 @@
+import { Session } from "@/lib/models/session";
+import { User } from "@/lib/models/user";
+import { connectToDB } from "@/lib/utils/db/connectToDB";
+import { cookies } from "next/headers";
+import { NextResponse } from "next/server";
+
+export async function GET() {
+	try {
+		const sessionId = cookies().get("sessionId")?.value;
+
+		if (!sessionId) {
+			return NextResponse.json({ authorized: false }, { status: 401 });
+		}
+
+		await connectToDB();
+
+		const session = await Session.findOne({ _id: sessionId });
+
+		if (!session || session.expiresAt < Date.now()) {
+			return NextResponse.json({ authorized: false }, { status: 401 });
+		}
+
+		const user = await User.findById(session.userId);
+
+		if (!user) {
+			return NextResponse.json({ authorized: false }, { status: 401 });
+		}
+
+		return NextResponse.json(
+			{ authorized: true, userId: user._id.toString() },
+			{ status: 200 }
+		);
+	} catch (error) {
+		console.error("Error while validating session", error);
+		return NextResponse.json({ authorized: false }, { status: 500 });
+	}
+}
